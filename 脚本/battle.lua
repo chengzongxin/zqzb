9c6d3a2fd44f1db62f9827d21b03df2000e89b54
chengzongxin <8876142@qq.com>
2025-04-27 22:42:11 +0800

    更新自动战斗逻辑以增强状态检测
    - 在`battle.lua`中修改了`autoFight`方法，新增了自动战斗状态检测逻辑，确保在不在自动战斗状态时能够正确点击自动战斗按钮。
    - 优化了滑动操作的等待时间，提升了操作的流畅性和用户体验。

--- 脚本/battle.lua
+++ 脚本/battle.lua
@@ -10,16 +10,23 @@ local Battle = {}
 -- 自动战斗
 function Battle:autoFight()
     print("开始自动战斗...")
-    -- 先左右滑动一下，失去自动战斗状态
-    swipe(210, 678, 70, 678, 200)
-    sleep(800)
-    swipe(210, 678, 350, 678, 200)
-    sleep(800)
-    -- 点击自动战斗按钮
-    tap(Config.coordinates.battle.autoFightButton.x, Config.coordinates.battle.autoFightButton.y)
-    sleep(1000)
+    -- 检测是否在自动战斗状态
+    setDict(0,"1.txt") --字库需要放到资源文件中
+    useDict(0)
+    local ret,x,y = findStr(589,526,1077,718,"战","FFFFC0-0F0F0F|FFFFDD-0F0F0F|FFFFA6-0F0F0F|FFFE94-0F0F0F|EBEBAD-0F0F0F|FDFDF1-0F0F0F|FFFFD1",0.50)
+    if x >=0 and y >= 0 then
+        print("在自动战斗状态 "..x.." "..y)
+    else
+        print("不在自动战斗状态，点击自动战斗按钮")
+        -- 先左右滑动一下，失去自动战斗状态
+        swipe(210, 678, 70, 678, 200)
+        sleep(500)
+        swipe(210, 678, 350, 678, 200)
+        sleep(500)
+        -- 点击自动战斗按钮
+        tap(Config.coordinates.battle.autoFightButton.x, Config.coordinates.battle.autoFightButton.y)
+        sleep(1000)
+    end
 end
 
-
-
-return Battle 
+return Battle
