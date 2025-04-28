e6ac8874d46782b4ef89abf5944b18eecae2d7ba
chengzongxin <8876142@qq.com>
2025-04-29 00:47:21 +0800

    back

--- 脚本/zqzb.lua
+++ 脚本/zqzb.lua
@@ -4,7 +4,7 @@ toast("最强战兵启动", 0, 0, 12)
 
 local ExchangeMoney = require("other/exchange_money")
 local exchangeMoney = ExchangeMoney:new()  -- 创建实例
--- exchangeMoney:start()  -- 调用实例方法
+--exchangeMoney:start()  -- 调用实例方法
 
 
 -- 导入模块
