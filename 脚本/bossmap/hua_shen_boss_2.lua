b5e0522b3f1b4f4935b4663bcce8339922d58f0b
chengzongxin <8876142@qq.com>
2025-04-29 21:49:08 +0800

    更新化身跨服BOSS的有效时间范围
    - 在`hua_shen_boss_2.lua`中，将有效时间范围从22点调整为21点，以适应新的游戏时间安排，确保玩家在更广泛的时间段内能够参与战斗。

--- 脚本/bossmap/hua_shen_boss_2.lua
+++ 脚本/bossmap/hua_shen_boss_2.lua
@@ -9,7 +9,7 @@ function HuaShenBoss2:new()
         fightTime = 15 * 60 * 1000,  -- 30分钟
         priority = 90,
         timeRestricted = true,
-        validTimeRange = {22, 23}  -- 12:00-14:00
+        validTimeRange = {21, 23}  -- 12:00-14:00
     })
     setmetatable(o, {__index = HuaShenBoss2})
     return o
